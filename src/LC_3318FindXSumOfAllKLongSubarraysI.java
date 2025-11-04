18aa76bd64e1224f7592b17d60922b9791ee4d6a
pratik <paithankarpratik3@gmail.com>
2025-11-04 14:24:03 +0530

    LC_3318FindXSumOfAllKLongSubarraysI done

---
+++ src/LC_3318FindXSumOfAllKLongSubarraysI.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class LC_3318FindXSumOfAllKLongSubarraysI {
+    public static void main(String[] args) {
+        int k=6,x=2;
+        int[] arr={1,1,2,2,3,4,2,3};
+        int ans[]=findXSum(arr,k,x);
+        System.out.println(Arrays.toString(ans));
+    }
+    public static int[] findXSum(int[] nums, int k, int x) {
+     int arr[]=new int[nums.length-k+1];
+     for(int i=0;i<=nums.length-k;i++){
+         HashMap<Integer,Integer> freq=new HashMap<>();
+         for(int j=i;j<i+k;j++){
+             freq.put(nums[j],freq.getOrDefault(nums[j],0)+1);
+         }
+//         iterating for the x elements
+         if(freq.size()<=x){
+             int sum=0;
+             for(int j=i;j<i+k;j++){
+                 sum+=nums[j];
+             }
+             arr[i]=sum;
+         }else{
+             List<Map.Entry<Integer,Integer>> pairs=new ArrayList<>(freq.entrySet());
+             pairs.sort((a,b)->{
+                 if(!a.getValue().equals(b.getValue())){
+                     return b.getValue() - a.getValue(); // higher freq first
+                 }else{
+                     return b.getKey() - a.getKey();     // higher value first
+                 }
+             });
+             int curSum=0;
+             for (int j = 0; j < x && j < pairs.size(); j++) {
+                 Map.Entry<Integer, Integer> e = pairs.get(j);
+                 curSum += e.getKey() * e.getValue();
+             }
+             arr[i] = curSum;
+         }
+         }
+    return arr;
+    }
+}
